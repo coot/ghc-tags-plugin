a1068a3fff4614365ab1a82c1ba98acca8fae94b
Marcin Szamotulski <profunctor@pm.me>
2020-02-22 22:02:52 +0100

    Simple CLI which generates tags
    Take s list of files and print vim tags to 'tags' file.  If a file does
    not parse list is to stdout.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,37 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import qualified MyLib (someFunc)
+import           Control.Monad (unless)
+import           Data.Traversable
+import           Data.Either
+import           System.Environment
+import           System.IO
+
+import qualified Data.ByteString.Builder as BS
+
+import           Lexer        ( ParseResult (..) )
+
+import           Language.Haskell.GHC.Tags
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  files <- getArgs
+  (mtags :: [Either String [GhcTag]]) <-
+    for files $ \file ->
+      parseGhcModuleIO file >>= \case
+        PFailed {}     -> return $ Left file
+        POk _ hsModule -> return $ Right $ generateTagsForModule hsModule
+
+  let 
+      tags :: GhcTags
+      (failed, tags) = concat <$> partitionEithers mtags
+
+  unless (null failed)
+    $ putStrLn $ "Failed to parse: " ++ show failed
+
+  putStrLn $ "found " ++ show (length tags) ++ " tags"
+
+  withFile "tags" WriteMode $ \handle ->
+    BS.hPutBuilder handle (foldMap formatGhcTagVim tags)
