74271c05a0e39765575479b282894f06c1285b16
Marcin Szamotulski <profunctor@pm.me>
2020-03-01 10:58:25 +0100

    Created a test-project

---
+++ test-project/lib/AnotherModule.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE GADTs #-}
+
+module AnotherModule where
+
+import MyLib
+
+-- | Either a block (@blk@) or a header (@'Header' blk@). Both have the same
+-- @HeaderHash blk@.
+data BlockOrHeader blk b where
+  Block  :: BlockOrHeader blk blk
+  Header :: BlockOrHeader blk (Header blk)
---
+++ test-project/lib/MyLib.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module MyLib where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+class MyLength a where
+    myLength :: a -> Int
+
+instance MyLength String where
+    myLength = length
+
+class GetHeader blk where
+  data family Header blk :: *
+  getHeader :: blk -> Header blk
+
+instance GetHeader Int where
+    data Header Int = HeaderInt Int
+    getHeader = HeaderInt
