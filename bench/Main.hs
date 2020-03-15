bc64a44dd2d1250d9a177d9e713c06aac97f0581
Marcin Szamotulski <profunctor@pm.me>
2020-03-15 06:41:53 +0100

    Bnechmark streaming with pipes against reading whole file
    The end-to-end benchmarks read a large tags file (1.5MB, 23k tags), and
    combines it with a relatively small tags files.
    
    * small:  389 tags,  24KB;
    * medium: 3034 tags, 230KB.
    
    These are likely the typical examples.
    
    In the small case the results are:
    
    * reading:   memory: 12MB, time 77.15ms
    * streaming: memory: 1MB,  time 86.92ms
    
    In the medium case:
    
    * reading:   memory: 15.8MB, time 80.82ms
    * streaming: memory: 4.4MB, time 92.93ms

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,25 +1,28 @@
 module Main (main) where
 
-import           Prelude
 import           Control.Exception
+import           Control.DeepSeq
+import           Control.Monad.State.Strict
 import qualified Data.ByteString         as BS
 import qualified Data.ByteString.Lazy    as BSL
 import qualified Data.ByteString.Builder as BB
+import           Data.Foldable (traverse_)
 import           Data.Text            (Text)
 import qualified Data.Text          as Text
 import qualified Data.Text.Encoding as Text
 import           System.IO
 
-import qualified Pipes            as Pipes
-import qualified Pipes.Attoparsec as Pipes.AP
-import qualified Pipes.ByteString as Pipes.BS
+import qualified Pipes               as Pipes
+import qualified Pipes.Attoparsec    as Pipes.AP
+import qualified Pipes.ByteString    as Pipes.BS
+import qualified Pipes.Text.Encoding as Pipes.Text
 
 import           Criterion
 import           Criterion.Main
 
 import           Plugin.GhcTags.Tag
-import           Plugin.GhcTags.Vim.Parser
-import           Plugin.GhcTags.Vim.Formatter
+import           Plugin.GhcTags.Stream
+import qualified Plugin.GhcTags.Vim as Vim
 
 evalList :: [a] -> ()
 evalList [] = ()
@@ -28,28 +31,34 @@ evalList (a : as) = a `seq` evalList as `seq` ()
 evalTags :: Either String [Tag] -> ()
 evalTags = either (`seq` ()) evalList
 
+newtype TagsNF = TagsNF [Tag]
+
+instance NFData TagsNF where
+    rnf (TagsNF tags) = evalList tags
+
+
 main :: IO ()
 main = defaultMain
   [ bgroup "Parse tags"
     [ -- 381 tags
       env (Text.decodeUtf8 <$> BS.readFile "test/golden/io-sim-classes.tags") $ \text ->
       bench "parse io-sim-classes.tags" $
-        whnfAppIO (fmap evalTags . parseTagsFile) text
+        whnfAppIO (fmap evalTags . Vim.parseTagsFile) text
 
     , -- 6767 tags
       env (Text.decodeUtf8 <$> BS.readFile "test/golden/ouroboros-consensus.tags") $ \text ->
       bench "parse ouroboros-consensus.tags" $
-        whnfAppIO (fmap evalTags . parseTagsFile) text
+        whnfAppIO (fmap evalTags . Vim.parseTagsFile) text
 
     , -- 12549 tags
       env (Text.decodeUtf8 <$> BS.readFile "bench/data.tags") $ \text ->
       bench "data.tags" $
-        whnfAppIO (fmap evalTags . parseTagsFile) text
+        whnfAppIO (fmap evalTags . Vim.parseTagsFile) text
 
     , -- 23741 tags
       env (Text.decodeUtf8 <$> BS.readFile "test/golden/vim.tags") $ \text ->
       bench "parse vim.tags" $
-        whnfAppIO (fmap evalTags . parseTagsFile) text
+        whnfAppIO (fmap evalTags . Vim.parseTagsFile) text
     ]
   , bgroup "read parse & format"
     [ bench "io-sim-classes.tags" $
@@ -71,16 +80,44 @@ main = defaultMain
     , bench "vim.tags" $
         nfIO $ benchStreamParseFormat "test/golden/vim.tags"
     ]
+    , bgroup "end-to-end"
+      [ env
+          (do
+            text <- Text.decodeUtf8 <$> BS.readFile "test/golden/io-sim-classes.tags"
+            Right tags  <- Vim.parseTagsFile text
+            return (TagsNF tags)
+          )
+          $ \ ~(TagsNF tags) ->
+            bgroup "small"
+              [ bench "streamTags" (whnfAppIO (benchStreamTags "test/golden/vim.tags") tags)
+
+              , bench "readTags" (whnfAppIO (benchReadTags "test/golden/vim.tags") tags)
+              ]
+      , env
+          (do
+            text <- Text.decodeUtf8 <$> BS.readFile "test/golden/ouroboros-network.tags"
+            Right tags  <- Vim.parseTagsFile text
+            return (TagsNF tags)
+          )
+          $ \ ~(TagsNF tags) ->
+            bgroup "medium"
+              [ bench "streamTags" (whnfAppIO (benchStreamTags "test/golden/vim.tags") tags)
+
+              , bench "readTags" (whnfAppIO (benchReadTags "test/golden/vim.tags") tags)
+              ]
+      ]
+
+
   ]
 
 
 benchReadParseFormat :: FilePath -> IO BSL.ByteString
 benchReadParseFormat path = do
     text <- Text.decodeUtf8 <$> BS.readFile path
-    res  <- parseTagsFile text
+    res  <- Vim.parseTagsFile text
     case res of
       Left err   -> throwIO (userError err)
-      Right tags -> pure $ BB.toLazyByteString (formatTagsFile tags)
+      Right tags -> pure $ BB.toLazyByteString (Vim.formatTagsFile tags)
 
 
 benchStreamParseFormat :: FilePath -> IO ()
@@ -89,10 +126,37 @@ benchStreamParseFormat fp =
       withFile fp ReadMode $ \h ->
         Pipes.void $ Pipes.runEffect $ Pipes.for
           (Pipes.AP.parsed
-            parseTag
+            Vim.parseTag
             (Pipes.BS.fromHandle h `Pipes.for` (Pipes.yield . Text.decodeUtf8)))
           (\tag -> 
-            (Pipes.BS.fromLazy . BB.toLazyByteString . formatTag) tag
+            (Pipes.BS.fromLazy . BB.toLazyByteString . Vim.formatTag) tag
             Pipes.>->
             Pipes.BS.toHandle devNull)
 
+benchStreamTags :: FilePath -> [Tag] -> IO ()
+benchStreamTags filePath tags =
+    withFile filePath ReadMode $ \readHandle -> 
+      withFile "/tmp/bench.stream.tags" WriteMode $ \writeHandle -> do
+        let producer :: Pipes.Producer Text IO ()
+            producer =
+              void $ Pipes.Text.decodeUtf8
+                           (Pipes.BS.fromHandle readHandle)
+
+            -- gags pipe
+            pipe :: Pipes.Effect (StateT [Tag] IO) ()
+            pipe =
+              Pipes.for
+                (Pipes.hoist Pipes.lift $ tagParser Vim.parseTagLine producer)
+                (runCombineTagsPipe writeHandle Vim.formatTag)
+        tags' <- execStateT (Pipes.runEffect pipe) tags
+        traverse_ (BSL.hPut writeHandle . BB.toLazyByteString . Vim.formatTag) tags'
+
+benchReadTags :: FilePath -> [Tag] -> IO ()
+benchReadTags filePath tags = do
+     withFile filePath ReadMode $ \readHandle -> 
+       withFile "/tmp/bench.stream.tags" WriteMode $ \writeHandle -> do
+         Right tags' <-
+           Text.decodeUtf8 <$> BS.hGetContents readHandle
+           >>= Vim.parseTagsFile
+         let tags'' = tags `combineTags` tags'
+         BB.hPutBuilder writeHandle (Vim.formatTagsFile tags'')
