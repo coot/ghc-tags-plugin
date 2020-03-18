5ff95c01e23d442a4003fc7bf11a8841c680b664
Marcin Szamotulski <profunctor@pm.me>
2020-03-18 20:08:51 +0100

    Handle Linux / MacOs & Windows line endings

--- lib/Plugin/GhcTags/Utils.hs
+++ lib/Plugin/GhcTags/Utils.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE CPP #-}
+
 module Plugin.GhcTags.Utils
-  ( withFileLock
+  ( endOfLine
+
+  , withFileLock
   , LockMode (..)
   ) where
 
@@ -7,6 +11,22 @@ import           Control.Exception
 import           System.IO
 import           GHC.IO.Handle
 import           GHC.IO.Handle.Lock
+
+
+-- | Platform dependend eol:
+--
+-- * windows      "CRNL"
+-- * maxos        "CR"
+-- * linux (unit) "NL"
+--
+endOfLine :: String
+#if defined(mingw32_HOST_OS)
+endOfLine = "\r\n"
+#elif defined(darwin_HIST_OS)
+endOfLine = "\r"
+#else
+endOfLine = "\n"
+#endif
 
 
 -- | 'flock' base lock (on posix) or `LockFileEx` on Windows.
