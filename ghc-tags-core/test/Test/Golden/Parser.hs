7f275fafc5659b2b0b448319eaf26620c20231d0
Marcin Szamotulski <profunctor@pm.me>
2020-03-28 09:31:00 +0100

    Golden tests
    Since we can parse headers now, we can test them in golden tests:
    
    * removed `goldenVsFileIgnoreHeaders`
    * added headers to some of the godlen tests

--- ghc-tags-core/test/Test/Golden/Parser.hs
+++ ghc-tags-core/test/Test/Golden/Parser.hs
@@ -2,27 +2,21 @@
 
 module Test.Golden.Parser (tests) where
 
-import           Algebra.Lattice ((/\))
-
 import           Control.Arrow
 import           Control.Exception
 import           Control.Monad ((>=>))
 
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString.Lazy.Char8 as BLC
 import qualified Data.ByteString.Builder as BS
 import qualified Data.Text.Encoding as Text
 import           System.IO
 import           System.FilePath
-import           Text.Printf
 
 import qualified GhcTags.CTag as CTag
 import qualified GhcTags.ETag as ETag
 
-import           Test.Tasty (TestTree, TestName, testGroup)
+import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.Golden
-import           Test.Tasty.Golden.Advanced
 
 
 tests :: FilePath -> TestTree
@@ -31,7 +25,7 @@ tests goldenTestDir =
       [ testGroup "CTag"
           [ let input  = goldenTestDir </> "test.tags"
                 output = goldenTestDir </> "test.tags.out"
-            in goldenVsFileIgnoreHeaders
+            in goldenVsFile
                 "test tags"
                  input
                  output
@@ -39,7 +33,7 @@ tests goldenTestDir =
 
           , let input  = goldenTestDir </> "vim.tags"
                 output = goldenTestDir </> "vim.tags.out"
-            in goldenVsFileIgnoreHeaders
+            in goldenVsFile
                 "vim tags"
                  input
                  output
@@ -79,7 +73,7 @@ tests goldenTestDir =
 
           , let input  = goldenTestDir </> "ghc.tags"
                 output = goldenTestDir </> "ghc.tags.out"
-            in goldenVsFileIgnoreHeaders
+            in goldenVsFile
                 "ghc tags"
                  input
                  output
@@ -140,45 +134,3 @@ parseGoldenETagsFile input output = do
       Right tags ->
         withBinaryFile output WriteMode
           $ flip BS.hPutBuilder (ETag.formatETagsFile tags)
-
---
---
---
-
--- | Compare the output file's contents against the golden file's contents
--- after the given action has created the output file.
-goldenVsFileIgnoreHeaders
-  :: TestName -- ^ test name
-  -> FilePath -- ^ path to the «golden» file (the file that contains correct output)
-  -> FilePath -- ^ path to the output file
-  -> IO () -- ^ action that creates the output file
-  -> TestTree -- ^ the test verifies that the output file contents is the same as the golden file contents
-goldenVsFileIgnoreHeaders name ref new act =
-  goldenTest
-    name
-    (readFileStrict ref)
-    (act >> readFileStrict new)
-    cmp
-    upd
-  where
-  upd = createDirectoriesAndWriteFile ref
-  cmp :: BSL.ByteString -> BSL.ByteString -> IO (Maybe String)
-  cmp b0 b1 = if filterHeaders b0 /= filterHeaders b1
-                then pure $ Just $ printf "Files '%s' and '%s' differ" ref new
-                else pure Nothing
-
-  -- filter headers, but only from the first 10 lnes
-  filterHeaders :: BSL.ByteString -> BSL.ByteString
-  filterHeaders = BLC.unlines
-                . uncurry (++)
-                . first (filter (not . BSL.isPrefixOf "!_TAG_FILE_"
-                              /\ not . BSL.isPrefixOf "!_TAG_PROGRAM_"))
-                . splitAt 20
-                . BLC.lines
-
-  
-readFileStrict :: FilePath -> IO BSL.ByteString
-readFileStrict path = do
-  s <- BSL.readFile path
-  evaluate $ BSL.foldr seq () s
-  return s
