38bbcf7dbd761202193b3487936765a0b382b1ef
Marcin Szamotulski <profunctor@pm.me>
2020-04-11 20:12:24 +0200

    Golden tests on windows

--- ghc-tags-core/test/Test/Golden/Parser.hs
+++ ghc-tags-core/test/Test/Golden/Parser.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Test.Golden.Parser (tests) where
@@ -17,90 +18,107 @@ import qualified GhcTags.ETag as ETag
 import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.Golden
 
+ext :: String
+#if !defined(mingw32_HOST_OS)
+ext = "posix"
+#else
+ext = "windows"
+#endif
+
 
 tests :: FilePath -> TestTree
 tests goldenTestDir =
     testGroup "Golden.Parser" $
       [ testGroup "CTag"
           [ let input  = goldenTestDir </> "test.tags"
+                golden = goldenTestDir </> "test.tags" <.> ext <.> "golden"
                 output = goldenTestDir </> "test.tags.out"
             in goldenVsFile
                 "test tags"
-                 input
+                 golden
                  output
                  (parseGoldenCTagsFile input output)
 
           , let input  = goldenTestDir </> "vim.tags"
+                golden = goldenTestDir </> "vim.tags" <.> ext <.> "golden"
                 output = goldenTestDir </> "vim.tags.out"
             in goldenVsFile
                 "vim tags"
-                 input
+                 golden
                  output
                  (parseGoldenCTagsFile input output)
 
           , let input  = goldenTestDir </> "typed-protocols.tags"
+                golden = goldenTestDir </> "typed-protocols.tags" <.> ext <.> "golden"
                 output = goldenTestDir </> "typed-protocols.tags.out"
             in goldenVsFile
                 "typed-protocols tags"
-                 input
+                 golden
                  output
                  (parseGoldenCTagsFile input output)
 
           , let input  = goldenTestDir </> "io-sim-classes.tags"
+                golden = goldenTestDir </> "io-sim-classes.tags" <.> ext <.> "golden"
                 output = goldenTestDir </> "io-sim-classes.tags.out"
             in goldenVsFile
                 "io-sim-classes tags"
-                 input
+                 golden
                  output
                  (parseGoldenCTagsFile input output)
          
           , let input  = goldenTestDir </> "ouroboros-network.tags"
+                golden = goldenTestDir </> "ouroboros-network.tags" <.> ext <.> "golden"
                 output = goldenTestDir </> "ouroboros-network.tags.out"
             in goldenVsFile 
                 "ouroboros-network tags"
-                 input
+                 golden
                  output
                  (parseGoldenCTagsFile input output)
 
           , let input  = goldenTestDir </> "ouroboros-consensus.tags"
+                golden = goldenTestDir </> "ouroboros-consensus.tags" <.> ext <.> "golden"
                 output = goldenTestDir </> "ouroboros-consensus.tags.out"
             in goldenVsFile
                 "ouroboros-consensus tags"
-                 input
+                 golden
                  output
                  (parseGoldenCTagsFile input output)
 
           , let input  = goldenTestDir </> "ghc.tags"
+                golden = goldenTestDir </> "ghc.tags" <.> ext <.> "golden"
                 output = goldenTestDir </> "ghc.tags.out"
             in goldenVsFile
                 "ghc tags"
-                 input
+                 golden
                  output
                  (parseGoldenCTagsFile input output)
           ]
 
       , testGroup "ETag"
           [ let input  = goldenTestDir </> "ouroboros-consensus.ETAGS"
+                golden = goldenTestDir </> "ouroboros-consensus.ETAGS" <.> ext <.> "golden"
                 output = goldenTestDir </> "ouroboros-consensus.ETAGS.out"
             in goldenVsFile 
                 "ouroboros-consensus TAGS"
-                 input
+                 golden
                  output
                  (parseGoldenETagsFile input output)
 
           , let input  = goldenTestDir </> "vim.ETAGS"
+                golden = goldenTestDir </> "vim.ETAGS" <.> ext <.> "golden"
                 output = goldenTestDir </> "vim.ETAGS.out"
             in goldenVsFile 
                 "vim tags"
-                 input
+                 golden
                  output
                  (parseGoldenETagsFile input output)
 
           , let input  = goldenTestDir </> "ghc.ETAGS"
+                golden = goldenTestDir </> "ghc.ETAGS" <.> ext <.> "golden"
                 output = goldenTestDir </> "ghc.ETAGS.out"
             in goldenVsFile 
                 "ghc tags"
-                 input
+                 golden
                  output
                  (parseGoldenETagsFile input output)
           ]
