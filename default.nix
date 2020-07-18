5662a69865c4e333f1f76dfabfa0814f71b718db
Marcin Szamotulski <profunctor@pm.me>
2020-07-19 00:09:03 +0200

    Use haskell.nix

--- default.nix
+++ default.nix
@@ -1,53 +1,39 @@
-{ compiler   ? "ghc865",
-  haddock    ? true,
-  test       ? true,
-  benchmarks ? false,
-  dev        ? true
+# Build with haskell.nix;
+#
+# nix-build -A ghc-tags-core
+# nix-build -A ghc-tags-plugin
+
+{ # Fetch the latest haskell.nix and import its default.nix
+  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
+
+# haskell.nix provides access to the nixpkgs pins which are used by our CI,
+# hence you will be more likely to get cache hits when using these.
+# But you can also just use your own, e.g. '<nixpkgs>'.
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+
+# haskell.nix provides some arguments to be passed to nixpkgs, including some
+# patches and also the haskell.nix functionality itself as an overlay.
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+
+# import nixpkgs with overlays
+, nixpkgs ? import nixpkgsSrc nixpkgsArgs
+, compiler ? "ghc865"
 }:
-with builtins;
-let
-  nixpkgs = import ./nix/nixpkgs.nix { inherit compiler; };
-
-  lib = nixpkgs.haskell.lib;
-  callCabal2nix = nixpkgs.haskell.packages.${compiler}.callCabal2nix;
-
-  doHaddock = if haddock
-    then lib.doHaddock
-    else lib.dontHaddock;
-  doTest = if test
-    then lib.doCheck
-    else lib.dontCheck;
-  doBench = if benchmarks
-    then lib.doBenchmark
-    else nixpkgs.lib.id;
-  doDev = if dev
-    then drv: lib.appendConfigureFlag drv "--ghc-option -Werror"
-    else nixpkgs.lib.id;
-  doEnableGtpCheck = if dev
-    then drv: lib.appendConfigureFlag drv "-f+gtp-check"
-    else nixpkgs.lib.id;
-  docNoSeprateOutput = drv: lib.overrideCabal drv (drv: { enableSeparateDocOutput = false; });
-
-  srcFilter = src: path: type:
-    let relPath = nixpkgs.lib.removePrefix (toString src + "/") (toString path);
-    in
-       nixpkgs.lib.hasPrefix "src"   relPath
-    || nixpkgs.lib.hasPrefix "lib"   relPath
-    || nixpkgs.lib.hasPrefix "test"  relPath
-    || nixpkgs.lib.hasPrefix "bench" relPath
-    || nixpkgs.lib.hasPrefix "app"   relPath
-    || nixpkgs.lib.any
-        (a: a == relPath)
-        [ "Setup.hs" "CHANGELOG.md" "ghc-tags-plugin.cabal" "ghc-tags-core.cabal" "LICENSE"];
-
-  ghc-tags-core = docNoSeprateOutput(doDev(doHaddock(doTest(doBench(
-    lib.overrideCabal (callCabal2nix "ghc-tags-core" ./ghc-tags-core {})
-      (drv: {src = nixpkgs.lib.cleanSourceWith { filter = srcFilter drv.src; src = drv.src; };})
-  )))));
-
-  ghc-tags-plugin = docNoSeprateOutput(doEnableGtpCheck(doDev(doHaddock(doTest(doBench(
-    lib.overrideCabal (callCabal2nix "ghc-tags-plugin" ./ghc-tags-plugin { inherit ghc-tags-core; })
-      (drv: {src = nixpkgs.lib.cleanSourceWith { filter = srcFilter drv.src; src = drv.src; };})
-  ))))));
-
-in { inherit ghc-tags-plugin ghc-tags-core; }
+let compiler-nix-name = compiler;
+    lib = nixpkgs.lib;
+    # 'cleanGit' cleans a source directory based on the files known by git
+    src = nixpkgs.haskell-nix.haskellLib.cleanGit {
+      name = "ghc-tags-plugin";
+      src = ./.;
+    };
+    projectPackages = lib.attrNames (nixpkgs.haskell-nix.haskellLib.selectProjectPackages
+      (nixpkgs.haskell-nix.cabalProject { inherit src compiler-nix-name; }));
+in nixpkgs.haskell-nix.cabalProject {
+      inherit src compiler-nix-name;
+      modules =
+        [ {
+            packages = lib.genAttrs projectPackages
+              (name: { configureFlags = [ "--ghc-option=-Werror" ]; });
+          }
+        ];
+    }
