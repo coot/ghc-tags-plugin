acd5c2757a46debc62d906f8b03f9db4b57eae1f
Marcin Szamotulski <profunctor@pm.me>
2020-07-19 16:39:03 +0200

    Use niv

--- default.nix
+++ default.nix
@@ -3,37 +3,44 @@
 # nix-build -A ghc-tags-core
 # nix-build -A ghc-tags-plugin
 
-{ # Fetch the latest haskell.nix and import its default.nix
-  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
-
-# haskell.nix provides access to the nixpkgs pins which are used by our CI,
-# hence you will be more likely to get cache hits when using these.
-# But you can also just use your own, e.g. '<nixpkgs>'.
-, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
-
-# haskell.nix provides some arguments to be passed to nixpkgs, including some
-# patches and also the haskell.nix functionality itself as an overlay.
-, nixpkgsArgs ? haskellNix.nixpkgsArgs
-
-# import nixpkgs with overlays
-, nixpkgs ? import nixpkgsSrc nixpkgsArgs
-, compiler ? "ghc865"
+{ compiler ? "ghc865"
 }:
 let compiler-nix-name = compiler;
-    lib = nixpkgs.lib;
+
+    sources = import ./nix/sources.nix {};
+    iohkNix = import sources.iohk-nix {};
+    haskellNix = import sources."haskell.nix" {};
+    nixpkgs = iohkNix.nixpkgs;
+    haskell-nix = haskellNix.pkgs.haskell-nix;
+
+    # package set
+    pkgs = import nixpkgs { config = haskellNix.config;
+                            overlays =
+                              [ (_: _: { inherit ghcTagsPluginPackages; }) ];
+                          };
+    lib = pkgs.lib;
+
     # 'cleanGit' cleans a source directory based on the files known by git
-    src = nixpkgs.haskell-nix.haskellLib.cleanGit {
+    src = haskell-nix.haskellLib.cleanGit {
       name = "ghc-tags-plugin";
       src = ./.;
     };
-    projectPackages = lib.attrNames (nixpkgs.haskell-nix.haskellLib.selectProjectPackages
-      (nixpkgs.haskell-nix.cabalProject { inherit src compiler-nix-name; }));
-in nixpkgs.haskell-nix.cabalProject {
-      inherit src compiler-nix-name;
-      modules =
-        [ {
-            packages = lib.genAttrs projectPackages
-              (name: { configureFlags = [ "--ghc-option=-Werror" ]; });
-          }
-        ];
-    }
+
+    # unmodified packages
+    projectPackages = lib.attrNames
+      (haskell-nix.haskellLib.selectProjectPackages
+      (haskell-nix.cabalProject { inherit src compiler-nix-name; }));
+
+    # set GHC options
+    ghcTagsPluginPackages = haskell-nix.cabalProject {
+        inherit src compiler-nix-name;
+        modules =
+          [
+            { packages =
+                lib.genAttrs
+                  projectPackages
+                  (name: { configureFlags = [ "--ghc-option=-Werror" ]; });
+            }
+          ];
+      };
+in pkgs.ghcTagsPluginPackages
